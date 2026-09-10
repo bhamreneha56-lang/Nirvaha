68231b836557ff485e3a22b116eda67954e81a68
Neha Dilip Bhamare <bhamreneha56@gmail.com>
2026-09-11 02:20:52 +0530

    tweak: slightly increase flag animation movement

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -17,10 +17,10 @@ export default function Landing() {
       {/* SVG Filter for Cloth Waving Effect */}
       <svg width="0" height="0" className="absolute pointer-events-none">
         <filter id="wave-filter">
-          <feTurbulence type="fractalNoise" baseFrequency="0.002 0.004" numOctaves="1" result="noise">
-            <animate attributeName="baseFrequency" values="0.002 0.004; 0.004 0.008; 0.002 0.004" dur="20s" repeatCount="indefinite" />
+          <feTurbulence type="fractalNoise" baseFrequency="0.003 0.006" numOctaves="1" result="noise">
+            <animate attributeName="baseFrequency" values="0.003 0.006; 0.005 0.01; 0.003 0.006" dur="16s" repeatCount="indefinite" />
           </feTurbulence>
-          <feDisplacementMap in="SourceGraphic" in2="noise" scale="12" xChannelSelector="R" yChannelSelector="G" />
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="18" xChannelSelector="R" yChannelSelector="G" />
         </filter>
       </svg>
 
